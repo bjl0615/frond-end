59ec10eeaf00d8f61af1dfcd26308a2e465539d7
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-12-24 23:40:42 +0900

    variable

---
+++ JS/day08/variable.js
@@ -0,0 +1,9 @@
+var book = "책";
+var book = "책", point = 123;
+var book = "책",
+    point = 123;
+var book = "책"
+    , point = 123;
+var point = 123, point = 456;
+point = 789;
+var point = amount = 123;
