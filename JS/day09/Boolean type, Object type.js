eea11a4df54c4ab197b75ce757ccfbd39c976c35
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-12-26 23:25:38 +0900

    Boolean type, Object type

---
+++ JS/day09/Boolean type, Object type.js
@@ -0,0 +1,15 @@
+console.log(true);
+console.log(false);
+
+var book = {
+    title: "책", point : 123
+};
+console.log(book);
+
+console.log(typeof 123);
+console.log(typeof "문자열");
+console.log(typeof true);
+console.log(typeof undefined);
+
+console.log(typeof null);
+console.log(typeof {book:"책"});
