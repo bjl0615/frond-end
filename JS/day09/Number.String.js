fd49d44d87cc53116486a9874cbb6e03743d881e
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-12-26 23:26:14 +0900

    Number,String

---
+++ JS/day09/Number.String.js
@@ -0,0 +1,15 @@
+var point = 123;
+console.log(typeof point);
+point = -1.23;
+console.log(typeof point);
+
+var point = 1 * "A";
+console.log(point);
+
+var point = "책, '123'";
+console.log(point);
+point = '책, "123"';
+console.log(point);
+
+var value = "123";
+console.log(typeof value);
