a041e7ac1242f3470495137ee824485545374772
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-12-28 23:06:30 +0900

    arithmetic operator

---
+++ JS/day10/arithmetic operator.js
@@ -0,0 +1,30 @@
+var value = 1 + 2 + 4;
+console.log(value);
+
+var two = "2";
+var value = 1 + two;
+console.log(value);
+console.log(typeof value);
+
+var value = 1 + 5 + "ABC";
+console.log(value);
+
+console.log("135" - 2);
+
+console.log(10* "20");
+console.log(10* true);
+console.log(10* false);
+console.log(10* null);
+console.log(10* "A");
+console.log(2.3 * 3);
+console.log(2.3 * 10 * 3 / 10);
+
+console.log(10 / "A");
+console.log(0 /0);
+console.log(0 / 3);
+console.log(3 / 0);
+
+console.log(5 % 2.5);
+console.log(5 % 2.3);
+console.log((5 * 10 - (2 * 2.3 * 10)) / 10);
+console.log(3 % 2);
