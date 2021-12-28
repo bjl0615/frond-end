83f1d78f3441bf5b73dcc14aa7a77f1e4a5e795a
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-12-28 23:07:49 +0900

    unary operator

---
+++ JS/day10/unary operator.js
@@ -0,0 +1,13 @@
+var value = "7";
+console.log(typeof value);
+console.log(typeof +value);
+console.log(typeof Number(value));
+
+function log(value) {
+    console.log(value);
+}
+
+var value = 7;
+console.log(-value);
+console.log(8 + -value);
+log(value);
