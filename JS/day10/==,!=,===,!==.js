81a7961d0bdf14932cf15ebd9f0faa7534f94897
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-12-28 23:06:09 +0900

    ==,!=,===,!==

---
+++ JS/day10/==,!=,===,!==.js
@@ -0,0 +1,15 @@
+var log = (value) => {console.log(value)}
+
+log(1 == "1");
+
+var value;
+log(undefined == value);
+
+var value;
+log(value == null);
+
+log(1 === "1");
+
+var value;
+log(value == null);
+log(value === null);
