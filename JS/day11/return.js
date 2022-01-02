1a5ec73ee41cffa7b58da6025149228bc4225b0f
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2022-01-02 22:41:45 +0900

    return

---
+++ JS/day11/return.js
@@ -0,0 +1,28 @@
+var log = function(value) {
+    console.log(value);
+}
+
+function setValue(one, two) {
+    var total = one + two;
+};
+
+setValue(10, 20);
+
+// function getPoint() {
+//     return 10 * 30;
+// };
+// var result = getPoint();
+// log(result);
+
+// function getPoint() {
+// };
+// var result = getPoint();
+// log(typeof result);
+
+function getPoint() {
+    return 
+    10 * 30
+};
+var result = getPoint();
+log(result);
+
