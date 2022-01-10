9635005fc91f3627fe4c9ff1b925d16e4d64dca5
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2022-01-10 22:08:41 +0900

    CharAy

---
+++ JS/day13/charAt.js
@@ -0,0 +1,14 @@
+var log = function(value){
+    console.log(value);
+}
+
+var value = "sports";
+log(value.charAt(1));
+log(value[1]);
+
+var value = "sports";
+log(value.charAt(12));
+
+var value = "sports";
+log(value[12]);
+
