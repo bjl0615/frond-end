802c6c0a3483f0a8a94d09fade5520755bf62fc0
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2022-03-14 23:45:20 +0900

    function_call

---
+++ JS/day15/function_call.js
@@ -0,0 +1,11 @@
+const log = (value) => {console.log(value)}
+
+log(Object.create);
+log(Object.prototype.create);
+
+log(Object.prototype.toString);
+
+var obh = {};
+log(obj.toString);
+
+log(String.fromCharCode(49,65));
